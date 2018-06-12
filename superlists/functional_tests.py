458b20199184c2b120755af5c1ebe990c3f8dd91
dfrd <fernandoarrj@gmail.com>
2018-06-11 22:13:46 -0300

    Adicionando novos testes funcionais

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith ouviu falar de uma nova aplicação online interessante para
 		# lista de tarefas. Ela decide verificar sua homepage
@@ -42,26 +47,33 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" como um item em uma lista de tarefas
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appar in table"
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
 
 		# Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro
 		# item. Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão)
 		# para fazer um fly - Edith é bem metódica)
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		# A pagina é atualizada novamente e agora mostra os dois itens em sua lista
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock to make a fly')
+
 
 		# Edith se pergunta se o site lembrará de sua lista. Então ela nota
 		# que o site gerou um URL único para ela -- há um pequeno
 		# texto explicativo par isso
+		self.fail('Finish the test!')
 
 		# Ela acessa esse URL - sua lista de tarefas continua lá.
+
 
 		# Satisfeita, ela volta a dormir
 
