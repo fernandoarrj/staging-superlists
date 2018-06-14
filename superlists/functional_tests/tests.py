3eaf8ea111ab1649c44cdeb81a116ba4a6dee020
dfrd <fernandoarrj@gmail.com>
2018-06-13 22:21:00 -0300

    Use Bootstrap to improve layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
@@ -10,7 +11,7 @@ import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.binary = FirefoxBinary('/opt/firefox/firefox')
